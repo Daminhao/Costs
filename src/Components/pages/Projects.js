a2d595a6dfd728897d779120fd93a65cda6121a3
daminhao <danilo.logic@gmail.com>
2022-04-22 18:32:40 -0300

    Finish feature remove project

--- src/Components/pages/Projects.js
+++ src/Components/pages/Projects.js
@@ -4,13 +4,17 @@ import { useState, useEffect } from 'react'
 
 import Message from '../layout/Message.js'
 import Container from '../layout/Container'
+import Loading from '../layout/Loading.js'
 import LinkButton from '../layout/LinkButton.js'
 import ProjectCard from '../project/ProjectCard'
 
 import styles from './Projects.module.css'
+import { remove } from 'lodash'
 
 function Projects() {
   const [projects, setProjects] = useState([])
+  const [removeLoading, setRemoveLoading] = useState(false)
+  const [projectMessage, setProjectMessage] = useState('')
 
   const location = useLocation()
   let message = ''
@@ -29,9 +33,26 @@ function Projects() {
       .then((resp) => resp.json())
       .then((data) => {
         setProjects(data)
+        setRemoveLoading(true)
       })
       .catch((err) => console.log(err))
   }, [])
+
+
+function removeProject(id){
+
+  fetch(`http://localhost:5000/projects/${id}`,{
+  method: 'DELETE',
+  headers: {
+    'Content-Type':'apllication/json' 
+  },
+}).then(resp => resp.json())
+  .then(data => {
+    setProjects(projects.filter((project) => project.id !== id))
+    setProjectMessage('Projeto removido com sucesso!')
+})
+
+}
 
   return (
   <div className={styles.project_container}>
@@ -48,9 +69,14 @@ function Projects() {
         name={project.name}
         budget={project.budget}
         category={project.category.name}
-        key={project.id}
+        key={project.id} 
+        handlerRemove={removeProject}
       />
       ))}
+      {!removeLoading && <Loading />}
+      {removeLoading && projects.lenght === 0 &&(
+        <p>Não há projetos cadastrados!</p>
+      )}
     </Container>
     </div>
   )
--- src/Components/project/ProjectCard.js
+++ src/Components/project/ProjectCard.js
@@ -2,9 +2,16 @@ import{Link} from 'react-router-dom'
 import styles from './ProjectCard.mudelo.css'
 
 import { Bspencil, BsFillTrashFill } from 'react-icons/bs'
-
+ 
 function ProjectCard({id, name, budget, category, handleRemove}) {
-		return (
+	
+const remove = (e) => {
+	e.preventDefault()
+	handleRemove(id)
+}
+
+	
+	return (
 		<div className = {styles.project_card}>
 			<h4> {name} </h4>
 			<p>
@@ -17,7 +24,7 @@ function ProjectCard({id, name, budget, category, handleRemove}) {
 			<Link to ="/">
 				 <BsPencil /> Editar
 					</Link>
-					<button>
+					<button onClick= {remove}>
 						<BssFillTrashFill /> 
 					</button>
 			</div>
